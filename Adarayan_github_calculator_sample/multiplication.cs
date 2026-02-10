98446643ace3b90917493a108e486f1ebba6553a
ShakingCan <adarayan.288066@balagtas.sti.edu.ph>
2026-02-10 19:55:00 +0800

    feat: added multiplication and division operation

---
+++ Adarayan_github_calculator_sample/multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adarayan_github_calculator_sample
+{
+    internal class multiplication
+    {
+        public int multiply(int num1, int num2)//gets two int values from the user
+        {
+            return num1 * num2;//returns the multiplied value of the two int
+        }
+    }
+}
