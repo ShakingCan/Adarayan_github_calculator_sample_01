b9c346dd94a4118ba55382447c2d157822facf82
meneses.306151 <meneses.306151@ANNEX-PC08>
2026-02-10 19:56:54 +0800

    Addition Function

---
+++ Adarayan_github_calculator_sample/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adarayan_github_calculator_sample
+{
+    internal class Addition
+    {
+        /// additional num1 + num2
+        public double addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
